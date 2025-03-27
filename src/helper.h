44d5bcc2ec1d854729e1e263a7396ad2d3c1073b
samuel koenigsraier <koenigsrainer.samuel@gmail.com>
2025-03-27 08:50:50 +0100

    secend commit

--- src/helper.h
+++ src/helper.h
@@ -2,6 +2,29 @@
 #define HELPER_H
 
 #include <Arduino.h>
+struct db
+{
+    bool vorher, out, in, inv;
+    unsigned long last = 0;
+    uint8_t taster = 0;
+    uint8_t db_zeit = 0;
 
+    void init(uint8_t, uint8_t, bool);
+
+    void poll();
+};
+struct flanke
+{
+public:
+    bool vorher, out, in, pos, neg, lang;
+    bool *input;
+
+private:
+    unsigned long zeit = 0;
+
+    void init(bool &);
+
+    void poll();
+};
 
 #endif
