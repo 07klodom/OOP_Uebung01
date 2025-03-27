53965b436cfd7de82ac59d8fe9957759469db4a4
samuel koenigsraier <koenigsrainer.samuel@gmail.com>
2025-03-27 09:10:19 +0100

    niomen isch niga

--- src/main.cpp
+++ src/main.cpp
@@ -25,14 +25,20 @@
 #include <Arduino.h>
 #include <analogblinker.h>
 #include <digitalblinker.h>
+#include <helper.h>
 #define LED1 8    // Low-side switch (leuchtet mit LOW)
-#define LED2 7  // Low-side switch (leuchtet mit LOW)
+#define LED2 7    // Low-side switch (leuchtet mit LOW)
 #define TASTER1 3 // LOW wenn gedrückt
 #define TASTER2 2 // LOW wenn gedrückt
-#define POT1 A7  
-uint32_t blinktime; 
+#define POT1 A7
+#define DB_ZEIT 5
+uint32_t blinktime;
 blinken analog;
 digital blink;
+db db_taster1;
+db db_taster2;
+flanke fp_taster1;
+flanke fp_taster2;
 bool enable, denable;
 void setup()
 {
@@ -40,10 +46,19 @@ void setup()
     Serial.println("..Start..\n");
     analog.init(LED1, LED2, POT1, enable);
     blink.init(LED1, LED2, blinktime, denable);
+    db_taster1.init(TASTER1, DB_ZEIT, false);
+    db_taster2.init(TASTER2, DB_ZEIT, true);
+    fp_taster1.init(db_taster1.out);
+    fp_taster2.init(db_taster2.out);
 }
 
 void loop()
 {
+    db_taster1.poll();
+    db_taster2.poll();
+    fp_taster1.poll();
+    fp_taster2.poll();
     analog.poll();
     blink.poll();
+
 }
